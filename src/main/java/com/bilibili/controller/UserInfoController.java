399bbff92abe04f81ba648fb2bd85a37de3b2a92
zpv2jdfc <zpv2jdfc@gmail.com>
2022-10-27 18:25:00 +0800

    个人空间

--- src/main/java/com/bilibili/controller/UserInfoController.java
+++ src/main/java/com/bilibili/controller/UserInfoController.java
@@ -2,7 +2,9 @@ package com.bilibili.controller;
 
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.interfaces.DecodedJWT;
+import com.bilibili.common.manager.Impl.RedisTokenManager;
 import com.bilibili.common.utils.JWTUtil;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,10 +15,14 @@ import javax.servlet.http.HttpSession;
 @RestController
 @RequestMapping("user")
 public class UserInfoController {
+
+    @Autowired
+    private RedisTokenManager tokenManager;
+
     @PostMapping(value = "/info")
     public String getUserInfo(HttpServletRequest request){
         String token = request.getHeader("token");
-        DecodedJWT decode = JWT.decode(token);
+        long id = tokenManager.getId(token);
         return "for test";
     }
 }
